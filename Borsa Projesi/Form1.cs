ff30ea955bb96ac99f425ee5690dd1402131e0b8
Burak Kızılay <40871181+burakkizilay46@users.noreply.github.com>
2021-05-10 17:13:17 +0300

    Login ve SignUp
    Kayıt Oluşturma ve Giriş Yapma Sistemleri Tamamlandı.
    BK..

--- Borsa Projesi/Form1.cs
+++ Borsa Projesi/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.SqlClient;
 
 namespace Borsa_Projesi
 {
@@ -20,6 +21,40 @@ namespace Borsa_Projesi
         private void Form1_Load(object sender, EventArgs e)
         {
 
+        }
+
+        SqlConnection con;
+        SqlCommand cmd;
+        SqlDataReader dr;
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            LoginPage pageLogin = new LoginPage();
+            pageLogin.Show();
+
+
+        }
+
+        private void btnGiris_Click(object sender, EventArgs e)
+        {
+            string userName = txtKullaniciAdi.Text;
+            string password = txtSifre.Text;
+
+            con = new SqlConnection("Data Source=DESKTOP-4UMSCIK;Initial Catalog=kullanici;Integrated Security=True");
+            cmd = new SqlCommand();
+            con.Open();
+            cmd.Connection = con;
+            cmd.CommandText = "SELECT * FROM kullanici where kullaniciadi = '" + txtKullaniciAdi.Text + "' AND sifre='" + txtSifre.Text.ToString() + "'";
+            dr = cmd.ExecuteReader();
+            if (dr.Read())
+            {
+                MessageBox.Show("Giris Yapildi");
+            }
+            else
+            {
+                MessageBox.Show("Yalnış Kullanici Adi veya Sifre!");
+            }
+            con.Close();
         }
     }
 }
